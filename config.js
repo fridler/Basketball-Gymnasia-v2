1d76e35a79d1947c54382dc7f0d2b75bd7d07291
Fridler <45431909+fridler@users.noreply.github.com>
2026-09-03 22:58:02 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,2 @@
-// const SUPABASE_URL = "https://kqygzdbbpenehefjtwvc.supabase.co";
-// const SUPABASE_ANON_KEY = "sb_publishable_S-JnocNz9VjJqqnHSv0Mwg__fvGy9qs";
-alert("alert");
+const SUPABASE_URL = "https://kqygzdbbpenehefjtwvc.supabase.co";
+const SUPABASE_ANON_KEY = "sb_publishable_S-JnocNz9VjJqqnHSv0Mwg__fvGy9qs";
